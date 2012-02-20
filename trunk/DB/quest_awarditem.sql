fecf4798b8835f582b09e81fed77b631b051ba03
anhlinhth@gmail.com <anhlinhth@gmail.com@f14765e7-a036-21f8-220b-26ebea3f6555>
2012-02-20 04:25:48 +0000

    DB

---
+++ trunk/DB/quest_awarditem.sql
@@ -0,0 +1,37 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Feb 20, 2012 at 04:42 AM
+-- Server version: 5.5.16
+-- PHP Version: 5.3.8
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `utit`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quest_awarditem`
+--
+
+CREATE TABLE IF NOT EXISTS `quest_awarditem` (
+  `QuestID` int(11) NOT NULL,
+  `AwardItem` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`QuestID`,`AwardItem`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
