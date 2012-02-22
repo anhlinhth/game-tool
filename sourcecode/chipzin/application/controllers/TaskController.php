086aabc21f2e78eb9ea7fc8c3803265ca14408c8
thaonx2890@gmail.com <thaonx2890@gmail.com@f14765e7-a036-21f8-220b-26ebea3f6555>
2012-02-22 04:37:31 +0000

    update TaskController.php, quest/edit.phtml

---
+++ sourcecode/chipzin/application/controllers/TaskController.php
@@ -0,0 +1,105 @@
+<?php
+require_once ROOT_APPLICATION_CONTROLLERS.DS.'BaseController.php';
+require_once ROOT_LIBRARY_UTILITY.DS.'utility.php';
+
+require_once ROOT_APPLICATION_MODELS.DS.'Models_Task.php';
+require_once ROOT_APPLICATION_MODELS.DS.'Models_Action.php';
+
+require_once ROOT_APPLICATION_MODELS.DS.'Models_Log.php';
+
+require_once ROOT_APPLICATION_OBJECT.DS.'Obj_Task.php';
+
+
+class TaskController extends BaseController
+{
+	public function _setUserPrivileges()
+	{
+		return array('index','add','edit','delete','active','item','additem','pigshop','itemshop');
+	}
+	
+	public function preDispatch()
+	{
+		parent::preDispatch();
+		
+		if(!$this->hasPrivilege())
+			$this->_redirect ("/error/permission");
+	}
+	
+	public function deleteAction()
+	{
+		try
+		{
+			$this->_helper->layout->disableLayout();
+			$this->_helper->viewRenderer->setNoRender();
+			$id = $this->_request->getParam("id"); //luu y cho nay nen dat id trung csd			
+			$mdquest = new Models_Quest();
+			$mdquest->_delete($id);	
+			Models_Log::insert($this->view->user->username, "act_delete_task");
+		}
+		catch(Exception $ex)
+        {            
+			$this->view->errMsg = $ex->getMessage();
+			Utility::log($ex->getMessage(), $ex->getFile(), $ex->getLine());
+        }
+	}
+	
+	
+	private function _getArrQuest()
+	{
+		$md = new Models_Task();
+		$Tasks = $md->_filter();
+		
+		$data = array();
+		if($Tasks)
+		{
+			foreach($Tasks as $task)
+			{
+				$data[] = array(
+					'TaskID' => $task->TaskID,
+					'TaskName' => $task->TaskName
+				);
+			}
+			
+			$this->view->arrTask = $data;
+		}
+	}	
+	
+	public function editAction()
+	{		
+		try
+		{
+			require_once ROOT_APPLICATION_FORMS.DS.'Forms_Task.php';			
+			//require_once ROOT_APPLICATION_FORMS.DS.'Forms_Action.php';
+			
+			$md = new Models_Task();
+			$mdAction = new Models_Action();
+			
+			if($this->_request->isPost())// da post du lieu len
+			{
+				
+			}
+			else{		// chua post du lieu-->load du lieu vao Form		
+				$id = $this->_request->getParam("id");
+				$this->view->obj = $md->_getByKey($id);
+				$this->view->arrAction = $mdAction->_getAction($this->view->obj->ActionID);
+				$this->view->objactionname = $mdAction->_getByKey($this->view->obj->ActionID);
+			}
+			
+		}
+		catch(Exception $ex)
+        {
+            $this->view->ojb = $form->obj;
+			$this->view->errMsg = $ex->getMessage();
+			Utility::log($ex->getMessage(), $ex->getFile(), $ex->getLine());
+        }
+		
+	}
+	
+	public function listquestAction()
+	{
+				
+	}
+	
+	
+}
+?>
