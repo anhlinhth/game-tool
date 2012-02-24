8bc65ca9234157f4a9341367706c076898d8515d
thaonx2890@gmail.com <thaonx2890@gmail.com@f14765e7-a036-21f8-220b-26ebea3f6555>
2012-02-24 02:38:07 +0000

    Thao

--- trunk/sourcecode/chipzin/application/views/helpers/ListQuestAwardItems.php
+++ trunk/sourcecode/chipzin/application/views/helpers/ListQuestAwardItems.php
@@ -9,7 +9,7 @@ class Zend_View_Helper_ListQuestAwardItems
 		{
 			//$key = $key+1;
 			$strList.= "<div class='awarditem' id='award_$key'><label></label><input name='awarditem[]' value='$row->AwardItem' id='' type='text' tabindex='1' />
-			<a title='Add Item' href='javascript:addAwardItem($key)'>Add</a>
+			
 			<a title='Delete Item' href='javascript:deleteAwardItem($key)'>Delete</a>
 			</div>";
 		}		
