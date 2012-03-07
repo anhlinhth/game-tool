e2c347d8cfc48b2d0e2d07d964955e8457ea21a7
Tan0330.UIT@gmail.com <Tan0330.UIT@gmail.com@f14765e7-a036-21f8-220b-26ebea3f6555>
2012-03-07 06:40:53 +0000

    OBJ Temp

---
+++ trunk/sourcecode/chipzin/application/object/Obj_Template.php
@@ -0,0 +1,15 @@
+<?php
+class Obj_Template
+{
+	public $TaskID;
+	public $TaskName;
+	public $TaskString;	
+	public $DescID;
+	public $DescString;
+	public $QTC_ID;
+	public $UnlockCoin;
+	public $IconClassName;
+	public $Quantity;
+	public $ActionID;
+	public $QuestID;
+}
