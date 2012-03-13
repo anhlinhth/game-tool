859695ccdb2d054edaf2280d3b4fc14df12cdb91
Tan0330.UIT@gmail.com <Tan0330.UIT@gmail.com@f14765e7-a036-21f8-220b-26ebea3f6555>
2012-03-13 10:04:58 +0000

    delete temp

--- trunk/sourcecode/chipzin/application/views/helpers/ListTemplate.php
+++ trunk/sourcecode/chipzin/application/views/helpers/ListTemplate.php
@@ -14,7 +14,7 @@ class Zend_View_Helper_ListTemplate
 			$delete = "";
 			$privilege = "";					
 				$edit = "<a href='$view->baseUrl/template/edit/id/$row->TaskID'><img src='$view->baseUrl/media/images/icons/edit-icon.gif' title='Chỉnh sửa' width='16' height='16' /></a>";
-				$delete = "<a href='javascript:deleteItem(\"$view->baseUrl/template/delete\",$row->QTC_ID)'><img src='$view->baseUrl/media/images/icons/delete.gif' title='Xóa' width='16' height='16' /></a>";								
+				$delete = "<a href='javascript:deleteItem(\"$view->baseUrl/template/delete\",$row->TaskID)'><img src='$view->baseUrl/media/images/icons/delete.gif' title='Xóa' width='16' height='16' /></a>";								
 			$strList .= "<tr id='$row->TaskID'>
 							<td class='descid'>$row->TaskID</td>	
 							<td class='desc'>$row->TaskName</td>		
