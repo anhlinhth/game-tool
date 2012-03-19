37152f8c3c75f13233f2c74006c7cd3530f73123
blackcat.it88 <blackcat.it88@f14765e7-a036-21f8-220b-26ebea3f6555>
2012-03-19 08:08:41 +0000

    final campaign

--- trunk/sourcecode/chipzin/application/modules/campaign/controllers/CampaignController.php
+++ trunk/sourcecode/chipzin/application/modules/campaign/controllers/CampaignController.php
@@ -1,8 +1,9 @@
 <?php
 require_once ROOT_APPLICATION_CONTROLLERS.DS.'BaseController.php';
-require_once ROOT_APPLICATION.DS.'modules'.DS.'campaign'.DS.'models'.DS.'Models_Campaign.php';
+require_once ROOT_APPLICATION.DS.'modules'.DS.'Campaign'.DS.'models'.DS.'Models_Campaign.php';
+require_once ROOT_APPLICATION.DS.'modules'.DS.'Campaign'.DS.'models'.DS.'Models_WorldMap.php';
 require_once ROOT_LIBRARY_UTILITY.DS.'utility.php';
-
+require_once ROOT_APPLICATION_MODELS.DS.'Models_Log.php';
 class Campaign_CampaignController extends BaseController
 {
 	public function _setUserPrivileges()
@@ -19,75 +20,154 @@ class Campaign_CampaignController extends BaseController
 	}
 	///////////////////////
 	public function indexAction(){
-		$md = new Models_Campaign();
-		echo $md->get();
-	}
-	////////////////////////////////////////////Edit///////////////////////////////////////////
-	public function editAction()
-	{
-		try 
+	try
 		{
-			require_once ROOT_APPLICATION.DS.'modules'.DS.'campaign'.DS.'models'.DS.'Models_Award.php';
-			require_once ROOT_APPLICATION.DS.'modules'.DS.'campaign'.DS.'models'.DS.'Models_Layout.php';
-			require_once ROOT_APPLICATION.DS.'modules'.DS.'campaign'.DS.'models'.DS.'Models_Battle.php';
-			require_once ROOT_APPLICATION.DS.'modules'.DS.'campaign'.DS.'models'.DS.'Models_Award_type.php';
-			require_once ROOT_APPLICATION.DS.'modules'.DS.'campaign'.DS.'models'.DS.'Models_Battle_Soldier.php';		
-			require_once ROOT_APPLICATION.DS.'modules'.DS.'campaign'.DS.'models'.DS.'Models_Soldier.php';	
+			require_once ROOT_APPLICATION.DS.'modules'.DS.'Campaign'.DS.'form'.DS.'Forms_Campaign.php';
+			$pageNo = $this->_request->getParam("page");
+			$items = $this->_request->getParam("items");
 			
-			$id = $this->_request->getParam("id");
+			if($pageNo == 0)
+				$pageNo = 1;
+			if($items == 0)
+				$items = DEFAULT_ITEM_PER_PAGE;
 			
-			///////Lấy Campaign//////////
-			$mdCamp = new Models_Campaign();			
-			$this->view->campaign = $mdCamp->_getByKey($id);
-			//var_dump($campaign);
-			
-			
-			///////Lấy danh sách Battle ///////////
-			//$mdbattle = new Models_Battle();
+			$md = new Models_Campaign();
+			$form= new Forms_Campaign();
+			$form->_requestToForm($this);
+			$data = $md->_filter($form->obj, "ID ASC", ($pageNo - 1)*$items, $items);
+			$count = $md->_count(null);
 			
 			
-			
-			///////Lấy danh sách Layout//////////
-			$mdlayout = new Models_Layout();
-			$this->view->arrlayout =  $mdlayout->getLayout();			
-			//print_r($this->view->arrlayout);
-			///////Lấy danh sách Award//////////
-			$mdawardtype = new Models_Award_Type();
-			$this->view->arrawardtype = $mdawardtype->getAwardtype();
-			////
+			$this->view->data = $data;
+			$this->view->items = $items;
+			$this->view->page = $pageNo;
+			$this->view->totalRecord = $count;
+		}
+		catch(Exception $ex)
+		{
+			$this->view->errMsg = $ex->getMessage();
+			Utility::log($ex->getMessage(), $ex->getFile(), $ex->getLine());
+		}
+	}
+	//////////////////////
+	public function editAction(){
 		
-			///////Đối với mỗi battle//////
-			$mdB_layout = new Models_Battle_Soldier();
+	}
+	public function deleteAction()
+	{
+	try
+		{
+			$this->_helper->layout->disableLayout();
+			$this->_helper->viewRenderer->setNorender();
 			
-			$arrbattle = $this->view->arrbattle;
-			$this->view->layout[] = array();
-			////////
-			$mdB_award = new Models_Award();
-			$this->view->arraward = array();
-			
-			foreach ($this->view->arrbattle as $row)
-			{
-				$idbattle= $row->ID;			
-			///////Lấy Award////////////
-				$this->view->arraward[$idbattle] = $mdB_award->getAward($idbattle);
+			if($this->_request->isPost())
+			{				
+				$id = $this->_request->getParam("id");								
+				$mdCamp = new Models_Campaign();
+
+				$obj= new Obj_Campaign();
+			//	$count=$mdTask->isExist($id);
 				
+				$mdCamp->delete((int)$id);
+				echo "Xóa QuestTask thành công";
+				Models_Log::insert($this->view->user->username, "act_delete_Campaign", $obj->name);													
 			}
-			//print_r($this->view->award);
-//			$this->view->battle_Layout[] = $layout[];
-//			$this->view->battle_award[] = $award[];
 		}
 		catch(Exception $ex)
         {            
 			$this->view->errMsg = $ex->getMessage();
 			Utility::log($ex->getMessage(), $ex->getFile(), $ex->getLine());
         }
-		
 	}
 	
+	
+	public function updateworldmapAction(){
+	try{
+		$this->_helper->layout->disableLayout();
+		$this->_helper->viewRenderer->setNorender();
+		$id = $this->_request->getParam("id");
+		$desc2 = $this->_request->getParam("desc2");	
+		$md = new Models_Campaign();	
+		$mdw = new Models_WorldMap();
+		
+		$obj = new Obj_Campaign();
+		$obj->ID = $id;
+		$obj->Name = $md->fetchname($id);
+		$obj->WorldMap = $mdw->searchname($desc2);
+		$md->update($obj);
+		Models_Log::insert($this->view->user->username, "act_update_Campaign", $obj->name);
+
+		echo "Update thanh cong";	
+		}
+		catch(Exception $ex)
+        {            
+			$this->view->errMsg = $ex->getMessage();
+			Utility::log($ex->getMessage(), $ex->getFile(), $ex->getLine());
+        }
+	}
+	public function updateAction(){
+	try{
+		$mdw = new Models_WorldMap();
+		$this->_helper->layout->disableLayout();
+		$this->_helper->viewRenderer->setNorender();
+		$id = $this->_request->getParam("id");
+		$desc = $this->_request->getParam("desc");
+		$wm = $this->_request->getParam("select");
+		$md = new Models_Campaign();	
+		$obj = new Obj_Campaign();
+		$obj->ID = $id;
+		$obj->Name = $desc;
+		if($wm==NULL)
+		{
+			echo "Chưa nhập dữ liệu listbox";
+			return;
+		}
+		else
+			$obj->WorldMap = $mdw->searchname($wm);
+	
+		$md->update($obj);
+		Models_Log::insert($this->view->user->username, "act_update_Campaign", $obj->name);
+		echo "Update thanh cong";	
+		}
+		catch(Exception $ex)
+        {            
+			$this->view->errMsg = $ex->getMessage();
+			Utility::log($ex->getMessage(), $ex->getFile(), $ex->getLine());
+        }
+	}
+	public function addAction(){
+	try{
+		$mdw = new Models_WorldMap();	
+		$this->_helper->layout->disableLayout();
+		$this->_helper->viewRenderer->setNorender();
+		$wm = $_POST[select];
+		$desc = $_POST[decs];
+		$obj = new Obj_Campaign();
+		if($wm==NULL)
+		{
+			echo "Chưa nhập dữ liệu listbox";
+			return;
+		}
+		else
+		$obj->WorldMap = $mdw->searchname($wm);
+		$obj->Name = $desc;
+		$md = new Models_Campaign();
+		try{
+			$md->insert($obj);
+		}
+		catch(Exception $ex)
+		{
+			var_dump($ex);
+			die();
+		}
+		Models_Log::insert($this->view->user->username, "act_insert_Campaign", $obj->name);
+
+		echo $obj->WorldMap;
+	}
+	catch(Exception $ex)
+        {            
+			$this->view->errMsg = $ex->getMessage();
+			Utility::log($ex->getMessage(), $ex->getFile(), $ex->getLine());
+        }
+	}
 }
-	
-	
-
-?>
-
-
