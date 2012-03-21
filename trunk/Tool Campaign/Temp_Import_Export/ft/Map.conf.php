b73e3a1e99c6326d3adf3d79d2a48d40d5b9134b
blackcat.it88 <blackcat.it88@f14765e7-a036-21f8-220b-26ebea3f6555>
2012-03-21 09:26:27 +0000

    ban moi cua anh Quoc

--- trunk/Tool Campaign/Temp_Import_Export/ft/Map.conf.php
+++ trunk/Tool Campaign/Temp_Import_Export/ft/Map.conf.php
@@ -3,12 +3,10 @@ return array
 (
 	MAP_1 => array
 	(
-		'type' => MAP,
-		'name' => 'Làng Bí Ẩn',
+		'type' => MAP, 
+		'name' => 'Làng Bí Ẩn', 
 		'needMap' => null,
 		'nextMap' => BARRACK_1,
-		'robBattle' => null,
-		'openBattles' => array(),
 		'width' => 40,
 		'heigh' => 40,
 		'blocks' => array(MAP_BLOCK_0 => null),
@@ -20,8 +18,6 @@ return array
 		'name' => 'Làng Bí Ẩn 2',
 		'needMap' => BARRACK_2,
 		'nextMap' => BARRACK_3,
-		'robBattle' => 1,
-		'openBattles' => array(1,2,3),
 		'width' => 40,
 		'heigh' => 40,
 		'blocks' => array(MAP_BLOCK_0 => null),
@@ -33,8 +29,6 @@ return array
 		'name' => 'Làng Bí Ẩn 3',
 		'needMap' => MAP_2,
 		'nextMap' => null,
-		'robBattle' => 4,
-		'openBattles' => array(4,5,6),
 		'width' => 40,
 		'heigh' => 40,
 		'blocks' => array(MAP_BLOCK_0 => null),
@@ -48,16 +42,12 @@ return array
 		'type' => BARRACK,
 		'needMap' => MAP_1,
 		'nextMap' => BARRACK_2,
-		'robBattle' => 1,
-		'openBattles' => array(1,2,3),
 	),
 	BARRACK_2 => array
 	(
 		'type' => BARRACK,
 		'needMap' => BARRACK_1,
 		'nextMap' => MAP_2,
-		'robBattle' => 1,
-		'openBattles' => array(4,5,6),
 	)
 	,
 	BARRACK_3 => array
@@ -65,8 +55,6 @@ return array
 		'type' => BARRACK,
 		'needMap' => MAP_2,
 		'nextMap' => BARRACK_4,
-		'robBattle' => 2,
-		'openBattles' => array(1,2,3),
 	)
 	,
 	BARRACK_4 => array
@@ -74,8 +62,6 @@ return array
 		'type' => BARRACK,
 		'needMap' => BARRACK_3,
 		'nextMap' => BARRACK_5,
-		'robBattle' => 4,
-		'openBattles' => array(4,5,6),
 	)
 	,
 	BARRACK_5 => array
@@ -83,32 +69,26 @@ return array
 		'type' => BARRACK,
 		'needMap' => BARRACK_4,
 		'nextMap' => MAP_3,
-		'robBattle' => 4,
-		'openBattles' => array(1,2,3),
 	),
 	BARRACK_6 => array
 	(
 		'type' => BARRACK,
 		'needMap' => BARRACK_5,
 		'nextMap' => BARRACK_7,
-		'robBattle' => 5,
-		'openBattles' => array(4,5,6),
+
 	),
 	BARRACK_7 => array
 	(
 		'type' => BARRACK,
 		'needMap' => BARRACK_6,
 		'nextMap' => BARRACK_8,
-		'robBattle' => 6,
-		'openBattles' => array(1,2,3),
+
 	),
 	BARRACK_8 => array
 	(
 		'type' => BARRACK,
 		'needMap' => BARRACK_7,
 		'nextMap' => MAP_3,
-		'robBattle' => 4,
-		'openBattles' => array(4,5,6),
 	)
 );
 ?>
