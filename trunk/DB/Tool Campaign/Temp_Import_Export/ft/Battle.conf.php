7fe011ba9b68b6c904cd163d767dec17db8b3248
anhlinhth@gmail.com <anhlinhth@gmail.com@f14765e7-a036-21f8-220b-26ebea3f6555>
2012-03-21 09:56:39 +0000

    add file export

---
+++ trunk/DB/Tool Campaign/Temp_Import_Export/ft/Battle.conf.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+'max_turn' => 100, 
+'random_min' => 1, 
+'random_max' => 2, 
+1 => array (
+'soldiers' => array (1 => array (SOLDIER_ENEMY_NORMAL_22, 1 ) ), 
+'awards' => array (GOLD => 100, WOOD => 10, IRON => 10 ) ), 
+
+);
