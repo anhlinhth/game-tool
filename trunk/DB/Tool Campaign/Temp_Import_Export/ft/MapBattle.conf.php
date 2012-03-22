3f622f0c7240ffcea6fb794566e3a9c1d4306320
anhlinhth@gmail.com <anhlinhth@gmail.com@f14765e7-a036-21f8-220b-26ebea3f6555>
2012-03-22 03:11:28 +0000

    fix file export

--- trunk/DB/Tool Campaign/Temp_Import_Export/ft/MapBattle.conf.php
+++ trunk/DB/Tool Campaign/Temp_Import_Export/ft/MapBattle.conf.php
@@ -6,61 +6,101 @@ return array
 		'robBattle' => null,
 		'openBattles' => array()
 	),
-	MAP_2 => array
-	(
-		'robBattle' => 1,
-		'openBattles' => array(1,2,3)
-	),
-	MAP_3 => array
-	(
-		'robBattle' => 4,
-		'openBattles' => array(4,5,6),
-	),
-	
-	/**********************************************************************/
-	
 	BARRACK_1 => array
 	(
 		'robBattle' => 1,
-		'openBattles' => array(1,2,3),
+		'openBattles' => array(1,2,3,4,5,6,7,8,9),
 	),
 	BARRACK_2 => array
 	(
-		'robBattle' => 1,
-		'openBattles' => array(4,5,6),
+		'robBattle' => 2,
+		'openBattles' => array(10,11,12,13,14,15,16,17,18),
 	)
 	,
 	BARRACK_3 => array
 	(
-		'robBattle' => 2,
-		'openBattles' => array(1,2,3),
+		'robBattle' => 3,
+		'openBattles' => array(19,20,21,22,23,24,25,26,27,28,29,30),
 	)
 	,
 	BARRACK_4 => array
 	(
 		'robBattle' => 4,
-		'openBattles' => array(4,5,6),
+		'openBattles' => array(31,32,33,34,35,36,37,38,39,40,41),
 	)
 	,
 	BARRACK_5 => array
 	(
-		'robBattle' => 4,
-		'openBattles' => array(1,2,3),
+		'robBattle' => 5,
+		'openBattles' => array(42,43,44,45,46,47,48,49,50,51,52,53),
 	),
 	BARRACK_6 => array
 	(
-		'robBattle' => 5,
-		'openBattles' => array(4,5,6),
+		'robBattle' => 6,
+		'openBattles' => array(54,55,56,57,58,59,60,61,62,63,64,65,66,67,68),
 	),
 	BARRACK_7 => array
 	(
-		'robBattle' => 6,
-		'openBattles' => array(1,2,3),
+		'robBattle' => 7,
+		'openBattles' => array(69,70,71,72,73,74,75,76),
 	),
 	BARRACK_8 => array
 	(
-		'robBattle' => 4,
-		'openBattles' => array(4,5,6),
+		'robBattle' => 8,
+		'openBattles' => array(77,78,79,80,81,82,83,84,85,86),
+	),
+	MAP_2 => array
+	(
+		'robBattle' => 9,
+		'openBattles' => array(87,88,89,90,91,92,93,94)
+	),
+	BARRACK_9 => array
+	(
+		'robBattle' => 10,
+		'openBattles' => array(95,96,97,98,99,100,101,102,103,104,105,106),
+	),
+	BARRACK_10 => array
+	(
+		'robBattle' => 11,
+		'openBattles' => array(107,108,109,110,111,112,113,114,115,116,117,118,119,120),
 	)
+	,
+	BARRACK_11 => array
+	(
+		'robBattle' => 12,
+		'openBattles' => array(121,122,123,124,125,126,127,128,129,130),
+	)
+	,
+	BARRACK_12 => array
+	(
+		'robBattle' => 13,
+		'openBattles' => array(131,132,133,134,135,136,137,138),
+	)
+	,
+	BARRACK_13 => array
+	(
+		'robBattle' => 14,
+		'openBattles' => array(139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154),
+	),
+	BARRACK_14 => array
+	(
+		'robBattle' => 15,
+		'openBattles' => array(155,156,157,158,159,160,161,162,163,164),
+	),
+	BARRACK_15 => array
+	(
+		'robBattle' => 16,
+		'openBattles' => array(165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182),
+	),
+	BARRACK_16 => array
+	(
+		'robBattle' => 17,
+		'openBattles' => array(183,184,185,186,187,188,189,190,191,192,193,194,195,196),
+	),
+	MAP_3 => array
+	(
+		'robBattle' => 18,
+		'openBattles' => array(197,198,199,200,201,202,203,204,205,206,207,208,209,210,211,212,213,214),
+	),
 );
 ?>
