d9e90d612360f09ede40cac0119478b65298571e
Tan0330.UIT@gmail.com <Tan0330.UIT@gmail.com@f14765e7-a036-21f8-220b-26ebea3f6555>
2012-03-27 04:56:57 +0000

    edit Layout

--- sourcecode/chipzin/application/modules/campaign/controllers/LayoutController.php
+++ sourcecode/chipzin/application/modules/campaign/controllers/LayoutController.php
@@ -3,6 +3,7 @@ require_once ROOT_APPLICATION_CONTROLLERS.DS.'BaseController.php';
 require_once ROOT_LIBRARY_UTILITY.DS.'utility.php';
 require_once ROOT_APPLICATION.DS.'modules'.DS.'campaign'.DS.'models'.DS.'Models_Layout.php';
 require_once ROOT_APPLICATION.DS.'modules'.DS.'campaign'.DS.'object'.DS.'Obj_Layout.php';
+
 class Campaign_LayoutController extends BaseController
 {
 	public function _setUserPrivileges()
@@ -38,6 +39,75 @@ class Campaign_LayoutController extends BaseController
 	    }
 		
 			
+	}
+	public function indexAction()
+	{
+		try
+		{
+			require_once ROOT_APPLICATION.DS.'modules'.DS.'campaign'.DS.'form'.DS.'Forms_Layout.php';
+			$pageNo = $this->_request->getParam("page");
+			$items = $this->_request->getParam("items");
+			$form = new Forms_Layout();
+			$form->_requestToForm($this);
+			$md = new Models_Layout();
+			$searchID=$this->_request->getParam('searchID');
+			$data = $md->filter($searchID, "ID ASC",($pageNo - 1)*$items, $items);
+			$count = $md->_count($form->obj);
+			
+			$this->view->data = $data;
+			$this->view->items = $items;
+			$this->view->page = $pageNo;
+			$this->view->totalRecord = $count;
+		}
+		catch(Exception $ex)
+		{
+			$this->view->errMsg = $ex->getMessage();
+			Utility::log($ex->getMessage(), $ex->getFile(), $ex->getLine());
+		}	
+	}
+	public function deleteAction()
+	{
+		try
+		{
+			$this->_helper->layout->disableLayout();
+			$this->_helper->viewRenderer->setNorender();
+			if($this->_request->isPost())
+			{
+				$id = $this->_request->getParam("ID");
+				$mdLayout = new Models_Layout();
+				$mdLayout->_delete($id);
+			//	Models_Log::insert($this->view->user->username, "act_delete_Layout", $obj->name);
+				echo 1;
+			}
+		}
+		catch(Exception $ex)
+		{
+			$this->view->errMsg = $ex->getMessage();
+			Utility::log($ex->getMessage(), $ex->getFile(), $ex->getLine());
+		}
+	}
+	public function updateAction()
+	{
+		try
+		{
+			$this->_helper->layout->disableLayout();
+			$this->_helper->viewRenderer->setNorender();
+			if($this->_request->isPost())
+			{
+				$id = $_POST['ID'];
+				$Point = $_POST['point'];
+				$mdLayout = new Models_Layout();
+				$obj = new Obj_Layout();
+				$mdLayout->update($id,$Point);
+				echo 1;
+			}
+		}
+		catch(Exception $ex)
+		{
+			echo $ex;
+			$this->view->errMsg = $ex->getMessage();
+			Utility::log($ex->getMessage(), $ex->getFile(), $ex->getLine());
+		}
 	}
 }
 
