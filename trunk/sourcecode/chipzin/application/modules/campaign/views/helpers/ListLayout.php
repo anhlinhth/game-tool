04719d1aac2e0fa4ed92cea55899a73209d77b34
Tan0330.UIT@gmail.com <Tan0330.UIT@gmail.com@f14765e7-a036-21f8-220b-26ebea3f6555>
2012-03-29 03:59:45 +0000

    edit Layout

--- trunk/sourcecode/chipzin/application/modules/campaign/views/helpers/ListLayout.php
+++ trunk/sourcecode/chipzin/application/modules/campaign/views/helpers/ListLayout.php
@@ -9,7 +9,23 @@ class Zend_View_Helper_ListLayout
 		$items = (($curPage - 1) * $itemPerPage) + 1;
 		$strList .= "<tbody>";
 		foreach($data as $row)
-		{					
+		{	
+			$arrCheck = array();
+			$flag = 0;
+			$i = 1;
+			while ($i < 18)
+			{
+				if($row->Point[$i] == '1')
+				{
+					$arrCheck[$flag] = 'checked';
+				}
+				else 
+				{
+					$arrCheck[$flag] = '';
+				}
+				$flag++;
+				$i += 2;
+			}
 			$edit = "";
 			$delete = "";
 			$privilege = "";					
@@ -18,7 +34,26 @@ class Zend_View_Helper_ListLayout
 			$strList .= "<tr id='$row->TaskID'>
 							<td class='descid'>$row->ID</td>	
 							<td class='desc'>$row->Name</td>		
-							<td class='desc'>$row->Point</td>						
+							<td class='desc'>
+								<table>
+									<tr>
+										<td>1<input type='checkbox' disabled $arrCheck[0] name='point$row->ID' id='0_$row->ID' value='0' /></td>
+										<td>4<input type='checkbox' disabled $arrCheck[3] name='point$row->ID' id='3_$row->ID' value='3' /></td>
+										<td>7<input type='checkbox' disabled $arrCheck[6] name='point$row->ID' id='6_$row->ID' value='6' /></td>
+									</tr>
+									<tr>
+										<td>2<input type='checkbox' disabled $arrCheck[1] name='point$row->ID' id='1_$row->ID' value='1' /></td>
+										<td>5<input type='checkbox' disabled $arrCheck[4] name='point$row->ID' id='4_$row->ID' value='4' /></td>
+										<td>8<input type='checkbox' disabled $arrCheck[7] name='point$row->ID' id='7_$row->ID' value='7' /></td>
+									</tr>
+									<tr>
+										<td>3<input type='checkbox' disabled $arrCheck[2] name='point$row->ID' id='2_$row->ID' value='2' /></td>
+										<td>6<input type='checkbox' disabled $arrCheck[5] name='point$row->ID' id='5_$row->ID' value='5' /></td>
+										<td>9<input type='checkbox' disabled $arrCheck[8] name='point$row->ID' id='8_$row->ID' value='8' /></td>
+									</tr>
+								</table>		
+							</td>
+											
 							<td align='center'>								
 								$edit&nbsp								
 								$delete								
@@ -28,7 +63,6 @@ class Zend_View_Helper_ListLayout
 		}
 		
 		$strList .= "</tbody>";		
-		
 		echo $strList;
 	}
 }
