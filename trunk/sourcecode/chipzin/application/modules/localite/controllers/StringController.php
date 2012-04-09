dba08f729b3347e061198483d73270acf88e50d3
thaonx2890@gmail.com <thaonx2890@gmail.com@f14765e7-a036-21f8-220b-26ebea3f6555>
2012-04-09 10:32:40 +0000

    add localite

---
+++ trunk/sourcecode/chipzin/application/modules/localite/controllers/StringController.php
@@ -0,0 +1,124 @@
+<?php
+require_once ROOT_APPLICATION_CONTROLLERS.DS.'BaseController.php';
+require_once ROOT_APPLICATION.DS.'modules'.DS.'campaign'.DS.'models'.DS.'Models_AwardType.php';
+require_once ROOT_LIBRARY_UTILITY.DS.'utility.php';
+require_once ROOT_APPLICATION_MODELS.DS.'Models_Log.php';
+require_once ROOT_APPLICATION.DS.'modules'.DS.'campaign'.DS.'object'.DS.'Obj_AwardType.php';
+require_once ROOT_APPLICATION.DS.'modules'.DS.'campaign'.DS.'form'.DS.'Forms_AwardType.php';
+
+require_once ROOT_APPLICATION.DS.'modules'.DS.'localite'.DS.'models'.DS.'Models_String.php';
+require_once ROOT_APPLICATION.DS.'modules'.DS.'localite'.DS.'models'.DS.'Models_Export.php';
+
+class Localite_StringController extends BaseController
+{
+	public function _setUserPrivileges()
+	{
+		return array('index','add','edit','update','delete');
+	}
+	public function preDispatch()
+	{
+		parent::preDispatch();
+		if(!$this->hasPrivilege())
+			$this->_redirect ("/error/permission");
+	}
+	public function indexAction()
+	{
+		$string = new Models_String();		
+		
+		if(isset($_REQUEST['l_language']))
+			$string->updateindexlang($_REQUEST['l_language']);
+		if(isset($_REQUEST['id'])&&isset($_REQUEST['group']))
+			$string->DelBykeyAndGroup($_REQUEST['id'], $_REQUEST['group']);
+			
+		$page = $this->_request->getParam("page",'1');
+		$size = $this->_request->getParam("size",'40');
+		
+		$l_group = $this->_request->getParam("l_group",-1);
+		$id_s = $this->_request->getParam("S_ID",-1);
+		if($id_s=='') $id_s=-1;
+		$l_Search_type = $this->_request->getParam("l_Search_type",'lkey');
+		
+		//replate
+		$this->view->data = $string->getDataByLang($l_group,$page,$size,$id_s,$l_Search_type);
+		$btreplace = $this->_request->getParam("btreplace",'0');
+		$replace = $this->_request->getParam("replace",-1);
+		$this->view->replace = $replace;
+		if($btreplace == "Replace")
+		{
+			if($id_s==-1||$replace==-1)
+				$this->view->errMsg = "Xin vui lòng điền đầy đủ thông tin";
+			foreach ($this->view->data as $value)
+			{
+				if($this->_request->getParam($value['id'],'0')=="on")
+					$string->updateReplace($value['id'], $id_s, $replace);
+			}
+		}
+		////
+		
+		$this->view->data = $string->getDataByLang($l_group,$page,$size,$id_s,$l_Search_type);
+		$this->view->datanopage =  $string->getDataByLangNopage($l_group,$id_s,$l_Search_type);
+		$this->view->page = $page;
+		$this->view->size = $size;
+		$this->view->group = $string->getGroup();
+		$this->view->language = $string->getlanguage();
+		$this->view->string = $string;
+		$this->view->l_group = $l_group;
+		$this->view->l_Search_type = $l_Search_type;
+		$this->view->id_s = $id_s;
+	}
+	public function insertAction()
+	{
+		$string = new Models_String();		
+		$this->view->group = $string->getGroup();
+		$this->view->string = $string;
+		$this->view->test = $this->_request->getParam("test",-1);
+		$this->view->test1 = $this->_request->getParam("test1",-1);
+		if(isset($_REQUEST['key'])&&isset($_REQUEST['l_group'])&&isset($_REQUEST['test'])&&isset($_REQUEST['test1']))
+		{
+			if($_REQUEST['key']==''||$_REQUEST['l_group']==''||$_REQUEST['test']==''||$_REQUEST['test1']=='')
+				$this->view->errMsg = "Xin vui lòng điền đầy đủ thông tin";
+			else
+			if(!$string->checkIdandGroup($_REQUEST['key'], $_REQUEST['l_group']))
+			{
+				$this->view->errMsg = "Bản dịch này có thể đã có trong CSDL.</br>Xin vui lòng kiểm tra lại!";
+			}
+			else 
+			{
+				$string->insert($_REQUEST['key'], $_REQUEST['l_group'], $_REQUEST['test'], $_REQUEST['test1']);
+				$this->_redirect('/localite/string/index');
+			}
+		}
+	}
+	public function updateAction()
+	{
+		$string = new Models_String();
+		$this->view->group = $string->getGroup();
+		$this->view->string = $string;
+		$this->view->test = $this->_request->getParam("test",-1);
+		$this->view->test1 = $this->_request->getParam("test1",-1);
+		if(isset($_REQUEST['key'])&&isset($_REQUEST['l_group'])&&isset($_REQUEST['test'])&&isset($_REQUEST['test1']))
+		{
+			if($_REQUEST['key']==''||$_REQUEST['l_group']==''||$_REQUEST['test']==''||$_REQUEST['test1']=='')
+				$this->view->errMsg = "Xin vui lòng điền đầy đủ thông tin";
+			else 
+			{
+				$string->update($_REQUEST['key'], $_REQUEST['group'], $_REQUEST['test'], $_REQUEST['test1']);
+				$this->_redirect('/localite/string/index');
+			}
+		}
+	}
+	public function testAction()
+	{
+		
+	}
+	public function viewkeyAction()
+	{
+		$string = new Models_String();
+		$model = new Models_Export();
+		$tam = $this->_request->getParam("value",'');
+		$lang = $string->getlangdefaultid();
+		if($tam!='')
+			echo '/n/'.$model->Replacekeyintext($model->gettextbykey($lang, $tam), $lang).'/n/';
+		else echo '/n//n/';
+	}
+}
---
+++ trunk/sourcecode/chipzin/application/modules/localite/models/Models_String.php
@@ -0,0 +1,201 @@
+<?php
+class Models_String
+{
+	var $db;
+	public function __construct()
+	{
+		$this->db = Zend_Registry::get("db");
+	}
+	public function updateReplace($id,$search, $replace)
+	{
+		$sql = "select text from l_content where id=".$id;
+		$tam = $this->db->fetchRow($sql);
+		$subject = $tam['text'];
+		$subjectrs = str_replace($search, $replace, $subject);
+		
+		$where = "id=".$id;
+		$data=array('text'=>$subjectrs);
+		$this->db->update('l_content',$data,$where);
+	}
+	public function getDataByLangNopage($l_group,$id_s,$l_Search_type)
+	{
+		$text ="";
+		if($l_group!=-1)
+			$text .= " and l_content.lgroup=".$l_group." ";
+		if($id_s!=-1)
+			$text .= " and ".$l_Search_type." like'%".$id_s."%' ";
+		
+			$sql = "SELECT l_content.id,l_content.lgroup,l_content.lkey,l_content.text,l_group.name
+					FROM l_group,l_content 
+					WHERE   l_group.id=l_content.lgroup ".$text." 
+					    	and lang in (select id from l_language where status = 1)"; 
+		return $this->db->fetchAll($sql);
+	}	
+	public function getDataByLang($l_group,$page,$count,$id_s,$l_Search_type)
+	{
+		$text ="";
+		if($l_group!=-1)
+			$text .= " and l_content.lgroup=".$l_group." ";
+		if($id_s!=-1)
+			$text .= " and ".$l_Search_type." like'%".$id_s."%' ";
+		
+			$sql = "SELECT l_content.id,l_content.lgroup,l_content.lkey,l_content.text,l_group.name
+					FROM l_group,l_content 
+					WHERE   l_group.id=l_content.lgroup ".$text." 
+					    	and lang in (select id from l_language where status = 1) 
+					limit ".($page-1)*$count.",".$count;
+		return $this->db->fetchAll($sql);
+	}
+	public function getGroup()
+	{
+		$sql = "select * from l_group";
+		return $this->db->fetchAll($sql);
+	}
+	public function getGroupbyid($id)
+	{
+		$sql = "select name from l_group where id=".$id;
+		$tam = $this->db->fetchRow($sql);
+		return $tam['name'];
+	}
+	public function getlanguage()
+	{
+		$sql = "select * from l_language where status <> 1 ORDER BY `l_language`.`status` DESC";
+		return $this->db->fetchAll($sql);
+	}
+	public function getlangdefault()
+	{
+		$sql = "select name from l_language where status = 1";
+		$tam = $this->db->fetchRow($sql);
+		return $tam['name'];
+	}
+	public function getlangindex()
+	{
+		$sql = "select name from l_language where status <> 1 ORDER BY `l_language`.`status` DESC";
+		$tam = $this->db->fetchRow($sql);
+		return $tam['name'];
+	}
+	public function getlangdefaultid()
+	{
+		$sql = "select id from l_language where status = 1";
+		$tam = $this->db->fetchRow($sql);
+		return $tam['id'];
+	}
+	public function getlangindexid()
+	{
+		$sql = "select id from l_language where status <> 1 ORDER BY `l_language`.`status` DESC";
+		$tam = $this->db->fetchRow($sql);
+		return $tam['id'];
+	}
+	public function getTextBylang($lang,$key,$group)
+	{
+		$sql = "select text from l_content where lang ='".$lang."' and lkey = '".$key."' and lgroup = ".$group;
+		$rs = $this->db->fetchRow($sql);
+		echo $rs['text'];
+	}
+	
+	public function updateindexlang($id)
+	{
+		$where="status=2";
+		$data=array('status'=>0);
+		$this->db->update('l_language',$data,$where);
+		$where="id='".$id."'";
+		$data=array('status'=>2);
+		$this->db->update('l_language',$data,$where);
+	}
+	public function DelBykeyAndGroup($key,$group)
+	{
+		$sql = 'delete l_content from l_content where lkey="'.$key.'" and lgroup='.$group;
+		$this->db->query($sql);
+	}
+	public function generateId(){
+		$sql = 'select lkey from l_content';
+		$result = $this->db->fetchCol($sql);
+		$bool=true;
+		$count = 1;
+		while ($bool == true){
+			$count;
+			if(!$this->checkid($count, $result)){
+				$bool = false;
+				return $count;				
+			}
+			$count++;
+		}
+		return $count;
+	}
+	public function checkid($id,$array){
+		foreach ($array as $value) {
+			if($value==$id){
+				return true;
+			}
+		}
+		return false;
+	}	
+	public function checkIdandGroup($key,$group)
+	{
+		$sql = "select id from l_content where lkey ='".$key."' and lgroup=".$group;
+		$rs = $this->db->fetchRow($sql);
+		if($rs['lkey']==$key)
+			return false;
+		else
+			return true;
+	}
+	
+	public function insert($key,$group,$text,$text1)
+	{
+		$sql = "select id from l_language where status = 1";
+		$tam = $this->db->fetchRow($sql);
+		$data = array(
+					'lkey'=>$key,
+					'lgroup'=>$group,
+					'text'=>$text,
+					'lang'=>$tam['id']
+					);
+		$this->db->insert('l_content',$data);
+		$sql = "select id from l_language where status <> 1 ORDER BY `l_language`.`status` DESC";
+		$tam2 = $this->db->fetchRow($sql);
+		$data = array(
+					'lkey'=>$key,
+					'lgroup'=>$group,
+					'text'=>$text1,
+					'lang'=>$tam2['id']
+					);
+		$this->db->insert('l_content',$data);
+		
+		$sql = "select id from l_language";
+		$tam3 = $this->db->fetchAll($sql);
+		foreach ($tam3 as $vl)
+		{
+			if($vl['id']!=$tam2['id']&&$vl['id']!=$tam['id'])
+			{
+				$data = array(
+					'lkey'=>$key,
+					'lgroup'=>$group,
+					'text'=>'',
+					'lang'=>$vl['id']
+					);
+				$this->db->insert('l_content',$data);
+				$sql = "select id from l_language where status = 1";
+				$tam3 = $this->db->fetchAll($sql);
+			}
+		}
+	}
+	public function update($key,$group,$text,$text1)
+	{
+		$sql = "select id from l_language where status = 1";
+		$tam = $this->db->fetchRow($sql);
+		$where = "lkey = '".$key."' and lgroup=".$group." and lang = '".$tam['id']."'";
+		$data = array(
+					'text'=>$text
+					);
+		$this->db->update('l_content',$data,$where);
+		
+		$sql = "select id from l_language where status <> 1 ORDER BY `l_language`.`status` DESC";
+		$tam2 = $this->db->fetchRow($sql);
+		$where = "lkey = '".$key."' and lgroup=".$group." and lang = '".$tam2['id']."'";
+		$data = array(
+					'text'=>$text1
+					);
+		$this->db->update('l_content',$data,$where);
+	}
+	
+}
