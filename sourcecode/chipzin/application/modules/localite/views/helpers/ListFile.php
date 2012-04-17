ee3ca2a62ccb464ec087c1355dc88fa4c6b613b8
anhlinhth@gmail.com <anhlinhth@gmail.com@f14765e7-a036-21f8-220b-26ebea3f6555>
2012-04-17 10:13:27 +0000

    add download

---
+++ sourcecode/chipzin/application/modules/localite/views/helpers/ListFile.php
@@ -0,0 +1,35 @@
+<?php
+class Zend_View_Helper_ListFile
+{
+	public function listFile($data, $curPage, $itemPerPage ,$view,$base)
+	{
+	
+		if(!$data)
+			return;
+		$items = (($curPage - 1) * $itemPerPage) + 1;
+		$strList .= "<tbody>";
+		foreach($data as $key =>$row)
+		{	
+			//var_dump($base)		;
+			//die();		
+			$edit = "";
+			$delete = "";
+			$privilege = "";	
+			$name= $row['Name'];
+			$time=$row['DateTime'];				
+			$urll="$base/Export/$name";			
+				$delete = "<a class='tool-24 delete' href='javascript:deleteres($key)'></a>";;	
+			$strList .= "<tr id='Type-$key' height=\"60\">
+							<td class='ID' >$items</td>
+							<td class='Name' id= 'Id-$key'  width=\"180px\"><a href=\"$urll\"".">$name</a></td>							
+							<td class='datatime'  width=\"180px\">$time</td>						
+							<td align='center'>	$delete	</td>
+						</tr>";
+			$items++;
+		}
+		
+		$strList .= "</tbody>";		
+		
+		echo $strList;
+	}
+}
