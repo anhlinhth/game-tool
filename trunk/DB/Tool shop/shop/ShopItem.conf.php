262cce92142e9631b90f9413509dabf4d7378514
anhlinhth@gmail.com <anhlinhth@gmail.com@f14765e7-a036-21f8-220b-26ebea3f6555>
2012-04-26 08:59:43 +0000

    Tool shop

---
+++ trunk/DB/Tool shop/shop/ShopItem.conf.php
@@ -0,0 +1,319 @@
+<?php
+return array
+(
+	CONFIG_DEFAULT_VALUE => array
+	(
+		'type' => null,
+		'buyPrice' => null,
+		'requireLevel' => null
+	),
+
+	/*********************Farm shop - Start************************/
+	10001 => array
+	(
+		'type' => ITEM_FARM_1,
+		'buyPrice' => array(GOLD => 10),
+		'requireLevel' => 1
+
+	),
+
+	10002 => array
+	(
+		'type' => ITEM_FARM_2,
+		'buyPrice' => array(GOLD => 15),
+		'requireLevel' => 3
+	),
+
+	10003 => array
+	(
+		'type' => ITEM_FARM_3,
+		'buyPrice' => array(GOLD => 30),
+		'requireLevel' => 5
+	),
+
+	10004 => array
+	(
+		'type' => ITEM_FARM_4,
+		'buyPrice' => array(GOLD => 45),
+		'requireLevel' => 6
+	),
+
+	10005 => array
+	(
+		'type' => ITEM_FARM_5,
+		'buyPrice' => array(GOLD => 75),
+		'requireLevel' => 7
+	),
+
+	10006 => array
+	(
+		'type' => ITEM_FARM_6,
+		'buyPrice' => array(GOLD => 90),
+		'requireLevel' => 8
+	),
+
+	10007 => array
+	(
+		'type' => ITEM_FARM_7,
+		'buyPrice' => array(GOLD => 115),
+		'requireLevel' => 9
+	),
+
+	10008 => array
+	(
+		'type' => ITEM_FARM_8,
+		'buyPrice' => array(GOLD => 150),
+		'requireLevel' => 12
+	),
+
+	10009 => array
+	(
+		'type' => ITEM_FARM_9,
+		'buyPrice' => array(GOLD => 300),
+		'requireLevel' => 15
+	),
+
+	10010 => array
+	(
+		'type' => ITEM_FARM_10,
+		'buyPrice' => array(COIN => 1),
+		'requireLevel' => 3
+	),
+
+	10011 => array
+	(
+		'type' => ITEM_FARM_11,
+		'buyPrice' => array(COIN => 2),
+		'requireLevel' => 3
+	),
+	10012 => array
+	(
+		'type' => ITEM_FARM_12,
+		'buyPrice' => array(COIN => 3),
+		'requireLevel' => 5
+	),
+	10013 => array
+	(
+		'type' => ITEM_FARM_13,
+		'buyPrice' => array(COIN => 4),
+		'requireLevel' => 5
+	),
+	10014 => array
+	(
+		'type' => ITEM_FARM_14,
+		'buyPrice' => array(COIN => 5),
+		'requireLevel' => 7
+	),
+	10015 => array
+	(
+		'type' => ITEM_FARM_15,
+		'buyPrice' => array(COIN => 6),
+		'requireLevel' => 7
+	),
+	/*********************Farm shop - End************************/
+
+	/*********************Wood shop - Start************************/
+	20001 => array
+	(
+		'type' => ITEM_WOOD_1,
+		'buyPrice' => array(),
+	),
+
+	20002 => array
+	(
+		'type' => ITEM_WOOD_2,
+		'buyPrice' => array(),
+	),
+
+	20003 => array
+	(
+		'type' => ITEM_WOOD_3,
+		'buyPrice' => array(),
+	),
+
+	20004 => array
+	(
+		'type' => ITEM_WOOD_4,
+		'buyPrice' => array(),
+	),
+
+	20005 => array
+	(
+		'type' => ITEM_WOOD_5,
+		'buyPrice' => array(),
+	),
+
+	20006 => array
+	(
+		'type' => ITEM_WOOD_6,
+		'buyPrice' => array(),
+	),
+
+	20007 => array
+	(
+		'type' => ITEM_WOOD_7,
+		'buyPrice' => array(),
+	),
+
+	20008 => array
+	(
+		'type' => ITEM_WOOD_8,
+		'buyPrice' => array(),
+	),
+	20009 => array
+	(
+		'type' => ITEM_WOOD_9,
+		'buyPrice' => array(),
+	),
+	/*********************Wood shop - End************************/
+
+	/*********************Iron shop - End************************/
+	20501 => array
+	(
+		'type' => ITEM_IRON_1,
+		'buyPrice' => array(),
+	),
+
+	20502 => array
+	(
+		'type' => ITEM_IRON_2,
+		'buyPrice' => array(),
+	),
+
+	20503 => array
+	(
+		'type' => ITEM_IRON_3,
+		'buyPrice' => array(),
+	),
+
+	20504 => array
+	(
+		'type' => ITEM_IRON_4,
+		'buyPrice' => array(),
+	),
+
+	20505 => array
+	(
+		'type' => ITEM_IRON_5,
+		'buyPrice' => array(),
+	),
+
+	20506 => array
+	(
+		'type' => ITEM_IRON_6,
+		'buyPrice' => array(),
+	),
+
+	20507 => array
+	(
+		'type' => ITEM_IRON_7,
+		'buyPrice' => array(),
+	),
+
+	20508 => array
+	(
+		'type' => ITEM_IRON_8,
+		'buyPrice' => array(),
+	),
+	20509 => array
+	(
+		'type' => ITEM_IRON_9,
+		'buyPrice' => array(),
+	),
+	/*********************Iron shop - End************************/
+
+	/*********************Stone shop - End************************/
+	21001 => array
+	(
+		'type' => ITEM_STONE_1,
+		'buyPrice' => array(),
+	),
+
+	21002 => array
+	(
+		'type' => ITEM_STONE_2,
+		'buyPrice' => array(),
+	),
+
+	21003 => array
+	(
+		'type' => ITEM_STONE_3,
+		'buyPrice' => array(),
+	),
+
+	21004 => array
+	(
+		'type' => ITEM_STONE_4,
+		'buyPrice' => array(),
+	),
+
+	21005 => array
+	(
+		'type' => ITEM_STONE_5,
+		'buyPrice' => array(),
+	),
+
+	21006 => array
+	(
+		'type' => ITEM_STONE_6,
+		'buyPrice' => array(),
+	),
+
+	21007 => array
+	(
+		'type' => ITEM_STONE_7,
+		'buyPrice' => array(),
+	),
+
+	21008 => array
+	(
+		'type' => ITEM_STONE_8,
+		'buyPrice' => array(),
+	),
+	21009 => array
+	(
+		'type' => ITEM_STONE_9,
+		'buyPrice' => array(),
+	),
+	/*********************Stone shop - End************************/
+
+	/*********************Military shop - Start************************/
+	21500 => array
+	(
+		'type' => ITEM_MILRESEARCH_1,
+		'buyPrice' => array(),
+	),
+
+	21501 => array
+	(
+		'type' => ITEM_MILRESEARCH_2,
+		'buyPrice' => array(),
+	),
+
+	21502 => array
+	(
+		'type' => ITEM_MILRESEARCH_3,
+		'buyPrice' => array(),
+	),
+
+	21503 => array
+	(
+		'type' => ITEM_MILRESEARCH_4,
+		'buyPrice' => array(),
+	),
+
+	21504 => array
+	(
+		'type' => ITEM_MILRESEARCH_5,
+		'buyPrice' => array(),
+	),
+
+	21505 => array
+	(
+		'type' => ITEM_MILRESEARCH_6,
+		'buyPrice' => array(),
+	),
+	/*********************Military shop - End************************/
+
+);
+?>
---
+++ trunk/DB/Tool shop/shop/ShopSoldier.conf.php
@@ -0,0 +1,472 @@
+<?php
+return array
+(
+	CONFIG_DEFAULT_VALUE => array
+	(
+		'type' => null,
+		'level' => null,
+		'buyPrice' => null,
+		'requireLevel' => null
+	),
+
+	/*********************Bo binh - Start************************/
+	30001 => array
+	(
+		'type' => SOLDIER_NORMAL_1,
+		'level' => 1,
+		'buyPrice' => array(GOLD => 20),
+	),
+
+	30002 => array
+	(
+		'type' => SOLDIER_ALL_1,
+		'level' => 5,
+		'buyPrice' => array(GOLD => 49,WOOD => 15),
+		'requireLevel' => 25
+	),
+
+	30003 => array
+	(
+		'type' => SOLDIER_NORMAL_2,
+		'level' => 10,
+		'buyPrice' => array(GOLD => 78, WOOD => 30),
+		'requireLevel' => 40
+	),
+
+	/*
+	30004 => array
+	(
+		'type' => SOLDIER_ROW_1,
+		'level' => 15,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30005 => array
+	(
+		'type' => SOLDIER_NORMAL_3,
+		'level' => 20,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30006 => array
+	(
+		'type' => SOLDIER_NORMAL_4,
+		'level' => 25,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30007 => array
+	(
+		'type' => SOLDIER_COL_1,
+		'level' => 30,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30008 => array
+	(
+		'type' => SOLDIER_NORMAL_5,
+		'level' => 35,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30009 => array
+	(
+		'type' => SOLDIER_NORMAL_6,
+		'level' => 40,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30010 => array
+	(
+		'type' => SOLDIER_ALL_2,
+		'level' => 45,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30011 => array
+	(
+		'type' => SOLDIER_NORMAL_7,
+		'level' => 50,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30012 => array
+	(
+		'type' => SOLDIER_NORMAL_8,
+		'level' => 55,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30013 => array
+	(
+		'type' => SOLDIER_ROW_2,
+		'level' => 60,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30014 => array
+	(
+		'type' => SOLDIER_NORMAL_9,
+		'level' => 65,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30015 => array
+	(
+		'type' => SOLDIER_NORMAL_10,
+		'level' => 70,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30016 => array
+	(
+		'type' => SOLDIER_NORMAL_11,
+		'level' => 75,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30017 => array
+	(
+		'type' => SOLDIER_COL_2,
+		'level' => 80,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30018 => array
+	(
+		'type' => SOLDIER_NORMAL_12,
+		'level' => 85,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30019 => array
+	(
+		'type' => SOLDIER_NORMAL_13,
+		'level' => 90,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30020 => array
+	(
+		'type' => SOLDIER_NORMAL_14,
+		'level' => 95,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30021 => array
+	(
+		'type' => SOLDIER_ALL_3,
+		'level' => 100,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+	*/
+	/*********************Bo binh - End************************/
+
+	/*********************Ky binh - Start************************/
+	30022 => array
+	(
+		'type' => SOLDIER_COL_3,
+		'level' => 1,
+		'buyPrice' => array(GOLD => 20),
+	),
+
+	30023 => array
+	(
+		'type' => SOLDIER_NORMAL_15,
+		'level' => 5,
+		'buyPrice' => array(GOLD => 49, IRON => 15),
+		'requireLevel' => 25
+
+	),
+
+	30024 => array
+	(
+		'type' => SOLDIER_NORMAL_16,
+		'level' => 10,
+		'buyPrice' => array(GOLD => 78, IRON => 30),
+		'requireLevel' => 40
+	),
+
+	/*
+	30025 => array
+	(
+		'type' => SOLDIER_ALL_4,
+		'level' => 15,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30026 => array
+	(
+		'type' => SOLDIER_NORMAL_17,
+		'level' => 20,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30027 => array
+	(
+		'type' => SOLDIER_NORMAL_18,
+		'level' => 25,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30028 => array
+	(
+		'type' => SOLDIER_ROW_3,
+		'level' => 30,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30029 => array
+	(
+		'type' => SOLDIER_NORMAL_19,
+		'level' => 35,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30030 => array
+	(
+		'type' => SOLDIER_NORMAL_20,
+		'level' => 40,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30031 => array
+	(
+		'type' => SOLDIER_NORMAL_21,
+		'level' => 45,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30032 => array
+	(
+		'type' => SOLDIER_COL_4,
+		'level' => 50,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30033 => array
+	(
+		'type' => SOLDIER_NORMAL_22,
+		'level' => 55,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30034 => array
+	(
+		'type' => SOLDIER_NORMAL_23,
+		'level' => 60,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30035 => array
+	(
+		'type' => SOLDIER_NORMAL_24,
+		'level' => 65,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30036 => array
+	(
+		'type' => SOLDIER_ALL_5,
+		'level' => 70,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30037 => array
+	(
+		'type' => SOLDIER_NORMAL_25,
+		'level' => 75,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30038 => array
+	(
+		'type' => SOLDIER_ALL_6,
+		'level' => 80,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30039 => array
+	(
+		'type' => SOLDIER_NORMAL_26,
+		'level' => 85,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30040 => array
+	(
+		'type' => SOLDIER_ROW_4,
+		'level' => 90,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30041 => array
+	(
+		'type' => SOLDIER_NORMAL_27,
+		'level' => 95,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30042 => array
+	(
+		'type' => SOLDIER_NORMAL_28,
+		'level' => 100,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+	*/
+	/*********************Ky binh - End************************/
+
+	/*********************Phao binh - Start************************/
+	30043 => array
+	(
+		'type' => SOLDIER_NORMAL_29,
+		'level' => 1,
+		'buyPrice' => array(GOLD => 20),
+	),
+
+	30044 => array
+	(
+		'type' => SOLDIER_NORMAL_30,
+		'level' => 5,
+		'buyPrice' => array(GOLD => 49, STONE => 15),
+		'requireLevel' => 25
+	),
+
+	30045 => array
+	(
+		'type' => SOLDIER_ROW_5,
+		'level' => 10,
+		'buyPrice' => array(GOLD => 78, STONE => 30),
+		'requireLevel' => 40
+	),
+
+	/*
+	30046 => array
+	(
+		'type' => SOLDIER_NORMAL_31,
+		'level' => 15,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30047 => array
+	(
+		'type' => SOLDIER_NORMAL_32,
+		'level' => 20,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30048 => array
+	(
+		'type' => SOLDIER_ALL_7,
+		'level' => 25,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30049 => array
+	(
+		'type' => SOLDIER_NORMAL_33,
+		'level' => 30,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30050 => array
+	(
+		'type' => SOLDIER_NORMAL_34,
+		'level' => 35,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30051 => array
+	(
+		'type' => SOLDIER_COL_5,
+		'level' => 40,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30052 => array
+	(
+		'type' => SOLDIER_NORMAL_35,
+		'level' => 45,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30053 => array
+	(
+		'type' => SOLDIER_NORMAL_36,
+		'level' => 50,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30054 => array
+	(
+		'type' => SOLDIER_COL_6,
+		'level' => 55,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30055 => array
+	(
+		'type' => SOLDIER_NORMAL_37,
+		'level' => 60,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30056 => array
+	(
+		'type' => SOLDIER_NORMAL_38,
+		'level' => 65,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30057 => array
+	(
+		'type' => SOLDIER_ALL_8,
+		'level' => 70,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30058 => array
+	(
+		'type' => SOLDIER_NORMAL_39,
+		'level' => 75,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30059 => array
+	(
+		'type' => SOLDIER_ROW_6,
+		'level' => 80,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30060 => array
+	(
+		'type' => SOLDIER_NORMAL_40,
+		'level' => 85,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30061 => array
+	(
+		'type' => SOLDIER_NORMAL_41,
+		'level' => 90,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30062 => array
+	(
+		'type' => SOLDIER_ALL_9,
+		'level' => 95,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+
+	30063 => array
+	(
+		'type' => SOLDIER_NORMAL_42,
+		'level' => 100,
+		'buyPrice' => array(GOLD => 100, IRON => 2),
+	),
+	*/
+	/*********************Phao binh - Start************************/
+);
+?>
