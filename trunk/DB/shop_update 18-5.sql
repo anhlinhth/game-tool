bcdcb5d3017f7b53e42616d65845cd3031fdce37
anhlinhth@gmail.com <anhlinhth@gmail.com@f14765e7-a036-21f8-220b-26ebea3f6555>
2012-05-18 10:19:47 +0000

    update bang du lieu : them bang type shop

---
+++ trunk/DB/shop_update 18-5.sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 18, 2012 at 12:18 PM
+-- Server version: 5.5.16
+-- PHP Version: 5.3.8
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `testthao`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `s_shop`
+--
+
+CREATE TABLE IF NOT EXISTS `s_shop` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) NOT NULL,
+  `TypeID` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `TypeID` (`TypeID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `s_shop`
+--
+
+INSERT INTO `s_shop` (`ID`, `Name`, `TypeID`) VALUES
+(1, 'stone_shop', 1),
+(2, 'wood_shop', 1),
+(3, 'farm_shop', 1),
+(4, 'mine_shop', 1),
+(5, 'soldier_shop_bobinh', 1),
+(6, 'soldier_shop_kibinh', 1),
+(7, 'soldier_shop_phaobinh', 1),
+(8, 'mil_research_shop', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `s_type_shop`
+--
+
+CREATE TABLE IF NOT EXISTS `s_type_shop` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `s_type_shop`
+--
+
+INSERT INTO `s_type_shop` (`ID`, `Name`) VALUES
+(1, 'Client'),
+(2, 'Server');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `s_shop`
+--
+ALTER TABLE `s_shop`
+  ADD CONSTRAINT `s_shop_ibfk_1` FOREIGN KEY (`TypeID`) REFERENCES `s_type_shop` (`ID`) ON DELETE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
